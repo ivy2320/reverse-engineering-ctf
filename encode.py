382303155ee599d6991d833159af4d5d98599928
Koushani <146692119+ivy2320@users.noreply.github.com>
2025-10-23 09:12:06 +0530

    Delete encode.py

--- encode.py
+++
@@ -1,8 +0,0 @@
-import base64
-flag = "flag{#mu@lt!_k3y_x0R_Solv3Rr!#}"
-reversed_flag = flag[::-1]
-b64_encoded = base64.b64encode(reversed_flag.encode())
-with open("challenge.bin", "wb") as f:
-    f.write(b64_encoded)
-
-print("[+] challenge.bin generated successfully!")
